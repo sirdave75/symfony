4627969e1df4597c5404435af92fb94dfb95560b
David Romanillos <david.romanillos@sildaviaweb.com>
2017-06-04 21:20:28 +0200

    Primer commit

---
+++ src/BackendBundle/Entity/Roles.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace BackendBundle\Entity;
+
+/**
+ * Roles
+ */
+class Roles
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $role;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set role
+     *
+     * @param string $role
+     *
+     * @return Roles
+     */
+    public function setRole($role)
+    {
+        $this->role = $role;
+
+        return $this;
+    }
+
+    /**
+     * Get role
+     *
+     * @return string
+     */
+    public function getRole()
+    {
+        return $this->role;
+    }
+}
+
